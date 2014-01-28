1046d10a111ad380c9cefd38f1adb2030c81e607
MatthieuBertin <matthieu.bertin@outlook.com>
2014-01-28 21:35:47 +0100

    signup

--- client.js
+++ client.js
@@ -11,31 +11,60 @@ window.onload = function() {
 		console.log("lol");
 		inputs.item(i).onchange = function() { console.log("ok"); this.style.border = "2px inset"; };
 	}
+	
 };
 
 
 
 
-var signup = function(formData) {
+function signUp() {
 
-	var msg = "";
+	var err = false;
 	
-	if (!formData.firstname.value) {
-		document.getElementById("firstname").style.border = "2px inset red";
+	// Check empty fields
+	var fields = document.forms["signup"].getElementsByTagName("input");
+	for (var i = 0; i < fields.length; i++) 
+		if (!fields.item(i).value) {
+			fields.item(i).style.border = "2px inset red";
+			err = true;
+		}
+	
+	// Check passwords
+	var pwd = document.forms["signup"]["pwd"];
+	var repwd = document.forms["signup"]["repwd"];
+	
+	if(err = pwd.value != repwd.value) 
+		pwd.value = repwd.value = "";
+
+	
+	if(!err) {		
+		var user = new Object();
+		user.email = document.forms["signup"]["email"].value;
+		user.password = document.forms["signup"]["pwd"].value;
+		user.firstname = document.forms["signup"]["firstname"].value;
+		user.familyname = document.forms["signup"]["familyname"].value;
+		user.gender = document.forms["signup"]["gender"].value;
+		user.city = document.forms["signup"]["city"].value;
+		user.country = document.forms["signup"]["country"].value;
 		
 		
-		//&& formData.familyname.value && formData.gender.value && formData.city.value && formData.country.value 
-		//&& formData.email.value && formData.pwd.value && formData.repwd.value)
-		
-		if(formData.pwd.value == formData.repwd.value)
-			console.log("all work");
-		else
-			msg = "Different password";
-	}	
-	else 
-		msg = "There is an empty field";
-		
-	document.getElementById("msg").innerHTML = msg;
+		var result = serverstub.signUp(user);
+		console.log(result);
+		if (!result.success) {
+		console.log("foiré");
+			document.forms["signup"]["email"].style.border = "2px inset red";
+			document.getElementById("msg").style.color = "red";
+			document.getElementById("msg").innerHTML = result.message;
+		} else {
+		console.log("ok");
+	/*		document.getElementById("msg").style.color = green;
+			document.getElementById("msg").innerHTML = result.message;
+			
+			var fields = document.forms["signup"].getElementsByTagName("input");
+			for (var i = 0; i < fields.length; i++) 
+				fields.item(i).value = "";*/
+		}
+	}
 };
 
 var login = function(formData) {
--- serverstub.js
+++ serverstub.js
@@ -111,6 +111,7 @@ serverstub.signOut = function(token){
 };
 serverstub.signUp = function(formData){ // {email, password, firstname, familyname, gender, city, country}
     if (users[formData.email] === undefined){
+		console.log("test");
         if(formData.email != undefined && formData.password != undefined && formData.firstname != undefined && formData.familyname != undefined && formData.gender != undefined && formData.city != undefined && formData.country != undefined){
             var user = {"email": formData.email,
                 "password": formData.password,
