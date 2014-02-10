4e3e2a117e892d52b635d143813e7c4aa41a6dc4
MatthieuBertin <matthieu.bertin@outlook.com>
2014-02-10 20:04:48 +0100

    json bug fixed

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ app.config['DEBUG'] = True
 def sign_in(email, password):
     pwd = hashlib.sha512(password.encode('utf-8'))
     if database_helper.check_user(email, pwd.digest()) is None:
-        return json.dump({"success": False, "message": "Wrong username or password."})
+        return json.dumps({"success": False, "message": "Wrong username or password."})
 
     rand = random.random()
     rand.seed()
@@ -21,30 +21,30 @@ def sign_in(email, password):
         token[i] = letters[rand.randint(0, len(letters)-1)]
 
     database_helper.signin_user(token, email)
-    return json.dump({"success": True, "message": "Successfully signed in.", "data": token})
+    return json.dumps({"success": True, "message": "Successfully signed in.", "data": token})
 
 
 @app.route('/sign_up/<email>/<password>/<firstname>/<familyname>/<gender>/<city>/<country>')
 def sign_up(email, password, firstname, familyname, gender, city, country):
 
     if(database_helper.user_exist(email)):
-        return json.dump({"success": False, "message": "User already exists."})
+        return json.dumps({"success": False, "message": "User already exists."})
 
     if(not email or not password or not firstname or not familyname or not gender or not city or not country):
-        return json.dump({"success": False, "message": "Formdata not complete."})
+        return json.dumps({"success": False, "message": "Formdata not complete."})
 
     database_helper.add_user(email, hashlib.sha512(password.encode('utf-8')).digest(), firstname, familyname, gender, city, country)
 
-    return json.dump({"success": True, "message": "Successfully created a new user."})
+    return json.dumps({"success": True, "message": "Successfully created a new user."})
 
 
 @app.route('/sign_out/<token>')
 def sign_out(token):
     if database_helper.user_signedin(token) is None:
-        return json.dump({"success": False, "message": "You are not signed in."})
+        return json.dumps({"success": False, "message": "You are not signed in."})
 
     database_helper.signout_user(token)
-    return json.dump({"success": True, "message": "Successfully signed out."})
+    return json.dumps({"success": True, "message": "Successfully signed out."})
 
 
 @app.route('/change_password/<token>/<old_password>/<new_password>')
@@ -52,13 +52,13 @@ def change_password(token, old_password, new_password):
 
     user = database_helper.user_signedin(token)
     if user is None:
-        return json.dump({"success": False, "message": "You are not logged in."})
+        return json.dumps({"success": False, "message": "You are not logged in."})
 
     if database_helper.get_password(user) != hashlib.sha512(old_password.encode('utf-8')).digest():
-        return json.dump({"success": False, "message": "Wrong password."})
+        return json.dumps({"success": False, "message": "Wrong password."})
 
     database_helper.update_password(user, hashlib.sha512(new_password.encode('utf-8')).digest())
-    return json.dump({"success": True, "message": "Password changed."})
+    return json.dumps({"success": True, "message": "Password changed."})
 
 
 @app.route('/get_user_messages_by_token/<token>')
@@ -69,13 +69,13 @@ def get_user_messages_by_token(token):
 @app.route('/get_user_messages_by_email/<token>/<email>')
 def get_user_messages_by_email(token, email):
     if email is None:
-        return json.dump({"success": False, "message": "You are not signed in."})
+        return json.dumps({"success": False, "message": "You are not signed in."})
 
     data = database_helper.get_messages(email)
     if data is None:
-        return json.dump({"success": False, "message": "No such user."})
+        return json.dumps({"success": False, "message": "No such user."})
 
-    return json.dump({"success": True, "message": "User messages retrieved..", "data": data})
+    return json.dumps({"success": True, "message": "User messages retrieved..", "data": data})
 
 
 @app.route('/get_user_data_by_token/<token>')
@@ -86,13 +86,13 @@ def get_user_data_by_token(token):
 @app.route('/get_user_data_by_email/<token>/<email>')
 def get_user_data_by_email(token, email):
     if email is None:
-        return json.dump({"success": False, "message": "You are not signed in."})
+        return json.dumps({"success": False, "message": "You are not signed in."})
 
     data = database_helper.get_user(email)
     if data is None:
-        return json.dump({"success": False, "message": "No such user."})
+        return json.dumps({"success": False, "message": "No such user."})
 
-    return json.dump({"success": True, "message": "User data retrieved..", "data": data})
+    return json.dumps({"success": True, "message": "User data retrieved..", "data": data})
 
 
 @app.route('/post_message/<token>/<message>/<email>')
@@ -100,13 +100,13 @@ def post_message(token, message, email):
 
     user = database_helper.user_signedin(token)
     if user is None:
-        return json.dump({"success": False, "message": "You are not signed in."})
+        return json.dumps({"success": False, "message": "You are not signed in."})
 
     if database_helper.user_exist(email) is None:
-        return json.dump({"success": False, "message": "No such user."})
+        return json.dumps({"success": False, "message": "No such user."})
 
     database_helper.post_message(user, email, message)
-    return json.dump({"success": True, "message": "Message posted"})
+    return json.dumps({"success": True, "message": "Message posted"})
 
 
 @app.teardown_appcontext
